ee80657db5e43cc974c6a865c5537d4b34c6a4a5
JayYarlott <98546364+JayYarlott@users.noreply.github.com>
2024-11-12 13:53:29 -0500

    broked it :P

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdlib.h>
 using namespace std;
 
 // Parameters: int vector arr, int vector reference removed elements
@@ -11,13 +12,14 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     sort(arr.begin(), arr.end());
     int n = arr.size();
     for(int i=0; i<n; i++){
-        if(i == 0 || arr[i] != arr[i-1]){
+        if(i == 0 || arr[i] == arr[i-1]){
             unique_arr.push_back(arr[i]); //adding elements to array of unique elements
         }
         else{
             removed_elements.push_back(arr[i]); //adding elements to array of duplicates
         }
     }
+    float* thing = malloc(120020123);
     return unique_arr;
 }
 
