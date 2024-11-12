d252ddf934c45781809df70538139dff8d88fa4c
csGoater <112995613+Bmcaleer1@users.noreply.github.com>
2024-11-12 13:45:38 -0500

    Update sum_even_fibonacci.py
    Comments on sum_even.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,7 +1,12 @@
+#initialize sequence
 a, b = 0, 1
+#set sum variable
 sum_even = 0
+#loop that goes until b is 4000000
 while b < 4000000:
+    #checks if its even
     if b % 2 == 0:
+        #add it to sum if it is
         sum_even += b
     a, b = b, a+b
 
