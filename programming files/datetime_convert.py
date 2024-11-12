844c21e6977c114a6b23289076cc48eeb0de1b18
esilver-uvm <112587294+esilver-uvm@users.noreply.github.com>
2024-11-12 13:59:02 -0500

    fix bad merge

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -3,12 +3,10 @@ from datetime import datetime
 
 # Date string
 date_str = "2022-03-17 10:45:30"
-patch-3
 # Date string to date object
 date_obj = datetime.strptime(date_str, '%m/%d/%Y %H:%M:%S')
 # Formatted date object to a reformatted string
 formatted_date = date_obj.strftime('%Y-%m-%d %H:%M:%S')
 
 # Prints formatted date
-main
 print(formatted_date)
