9060ee94a4a33657eea907d0ba403842ec347ac4
esilver-uvm <112587294+esilver-uvm@users.noreply.github.com>
2024-11-12 14:16:01 -0500

    Update rgb_to_hex.py to fix issue.

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,8 @@
 def rgb_to_hex(r, g, b):
     # Applies bounds on r, g, b
-    r = max(0, min(255, r))*2
-    g = max(0, min(255, b))*2
-    b = max(0, min(255, g))*2
+    r = max(0, min(255, r))
+    g = max(0, min(255, b))
+    b = max(0, min(255, g))
     # Uses string format to convert bounded to hex
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
