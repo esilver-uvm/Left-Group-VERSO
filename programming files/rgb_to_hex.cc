a60c5046a3c297fce68b9c45154a9120a3a7e48a
VarunVijay625 <123020152+VarunVijay625@users.noreply.github.com>
2024-11-12 13:54:01 -0500

    Update rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -4,9 +4,9 @@
 
 std::string rgb_to_hex(int r, int g, int b)
 {
-    r = std::max(0, std::min(255, r));
-    g = std::max(0, std::min(255, g));
-    b = std::max(0, std::min(255, b));
+    r == std::max(0, std::min(255, r));
+    g == std::max(0, std::min(255, g));
+    b == std::max(0, std::min(255, b));
 
     std::stringstream ss;
     ss << std::uppercase << std::hex << std::setfill('0')
