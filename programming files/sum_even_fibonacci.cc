20ce6cd52118cda45a3da8bc0581a93bce7c6bbb
JayYarlott <98546364+JayYarlott@users.noreply.github.com>
2024-11-12 13:38:45 -0500

    Comment sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -2,16 +2,22 @@
 
 int main()
 {
+    //initial state of fibonacci, 0, 1
     int a = 0, b = 1;
     int sum_even = 0;
+    //loop through 4 million iterations 
     while (b < 4000000) {
+        //if iteration is even, add current fibbonocci to sum
         if (b % 2 == 0) {
             sum_even += b;
         }
+
+        //iterate fibbonocci 
         int temp = b;
         b = a + b;
         a = temp;
     }
+    //print out sum
     std::cout << sum_even << std::endl;
 
     return 0;
