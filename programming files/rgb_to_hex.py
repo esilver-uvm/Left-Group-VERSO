cd9f7bacfd735eeb4dcdad257d4f29114ef4ab7f
ryguysyms <112989251+ryguysyms@users.noreply.github.com>
2024-11-12 13:52:01 -0500

    added bugs to rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,8 @@
 def rgb_to_hex(r, g, b):
     # Applies bounds on r, g, b
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    r = max(0, min(255, r))*2
+    g = max(0, min(255, b))*2
+    b = max(0, min(255, g))*2
     # Uses string format to convert bounded to hex
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
