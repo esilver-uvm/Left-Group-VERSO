748f72f70eeca6c0c9e0b24f9c4ed6ce346f3352
esilver-uvm <112587294+esilver-uvm@users.noreply.github.com>
2024-11-12 13:44:29 -0500

    Document even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -4,9 +4,12 @@ using namespace std;
 
 int main() {
     ofstream file;
+    // Open a file to write
     file.open("numbers.html");
+    // Define the header for a table in html
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    // Iterate through numbers <= 50, put evens under even, odds under odds.
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
@@ -16,7 +19,9 @@ int main() {
         }
     }
     file << "</table>\n</body>\n</html>";
+    // Close file.
     file.close();
+    // Print the file then close.
     ifstream input("numbers.html");
     cout << input.rdbuf();
     input.close();
